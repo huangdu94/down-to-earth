9f197678244c3daab58a953e9c70c1d5d76e4682
huangdu94 <duhuang@iflytek.com>
2019-09-28 17:34:48 +0800

    update

---
+++ SpringCloud学习/code/spring-cloud-demo/config-server/src/main/java/com/iflytek/springclouddemo/config/server/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.iflytek.springclouddemo.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Spring Cloud Config Server
+ * 2019/9/28 17:15
+ *
+ * @author DuHuang
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
