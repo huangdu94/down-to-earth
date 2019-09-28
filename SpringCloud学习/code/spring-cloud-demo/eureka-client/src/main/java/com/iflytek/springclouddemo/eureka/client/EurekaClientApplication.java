05b6d2fd65f670f79264112d13e398e0fb800f82
huangdu94 <duhuang@iflytek.com>
2019-09-29 00:19:09 +0800

    Add modules. eureka-server and eureka-client as eureka demo.

---
+++ SpringCloud学习/code/spring-cloud-demo/eureka-client/src/main/java/com/iflytek/springclouddemo/eureka/client/EurekaClientApplication.java
@@ -0,0 +1,10 @@
+package com.iflytek.springclouddemo.eureka.client;
+
+/**
+ * Eureka Client
+ * 2019/9/29 0:16
+ *
+ * @author DuHuang
+ */
+public class EurekaClientApplication {
+}
---
+++ SpringCloud学习/code/spring-cloud-demo/eureka-server/src/main/java/com/iflytek/springclouddemo/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,10 @@
+package com.iflytek.springclouddemo.eureka.server;
+
+/**
+ * Eureka Server
+ * 2019/9/29 0:16
+ *
+ * @author DuHuang
+ */
+public class EurekaServerApplication {
+}
