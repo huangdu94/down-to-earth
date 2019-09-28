6bbe90a82f448bed50bb24b0e99996f6c2ae45c8
huangdu94 <duhuang@iflytek.com>
2019-09-28 17:44:14 +0800

    code update.

---
+++ SpringCloud学习/code/spring-cloud-demo/config-client/src/main/java/com/iflytek/springclouddemo/config/client/ConfigClientApplication.java
@@ -0,0 +1,11 @@
+package com.iflytek.springclouddemo.config.client;
+
+/**
+ * Spring Cloud Config Client
+ * 2019/9/28 17:37
+ *
+ * @author DuHuang
+ */
+
+public class ConfigClientApplication {
+}
