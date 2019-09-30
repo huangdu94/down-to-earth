65de5faabb714fc2cc0963a63434134c01d245c6
huangdu94 <duhuang@iflytek.com>
2019-09-30 09:50:18 +0800

    Update modules. eureka-server and eureka-client as eureka demo.

--- SpringCloud学习/code/spring-cloud-demo/eureka-client/src/main/java/com/iflytek/springclouddemo/eureka/client/EurekaClientApplication.java
+++ SpringCloud学习/code/spring-cloud-demo/eureka-client/src/main/java/com/iflytek/springclouddemo/eureka/client/EurekaClientApplication.java
@@ -1,10 +1,33 @@
 package com.iflytek.springclouddemo.eureka.client;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Eureka Client
  * 2019/9/29 0:16
  *
  * @author DuHuang
  */
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class EurekaClientApplication {
+    @Value("${server.port}")
+    private String port;
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @RequestMapping("/{name}")
+    public String home(@PathVariable String name, @RequestParam(value = "message", defaultValue = "Hello World") String message) {
+        return String.format("Come by port: %s,say to %s: %s.", port, name, message);
+    }
 }
--- SpringCloud学习/code/spring-cloud-demo/eureka-server/src/main/java/com/iflytek/springclouddemo/eureka/server/EurekaServerApplication.java
+++ SpringCloud学习/code/spring-cloud-demo/eureka-server/src/main/java/com/iflytek/springclouddemo/eureka/server/EurekaServerApplication.java
@@ -1,10 +1,19 @@
 package com.iflytek.springclouddemo.eureka.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * Eureka Server
  * 2019/9/29 0:16
  *
  * @author DuHuang
  */
+@SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
 }
