27e9765f399c0d45777c3eb7dec9f00073ddc1ff
huangdu94 <duhuang@iflytek.com>
2019-10-03 22:06:36 +0800

    add zuul

---
+++ SpringCloud学习/code/spring-cloud-demo/service-zuul/src/main/java/com/iflytek/springclouddemo/zuul/ZuulApplication.java
@@ -0,0 +1,23 @@
+package com.iflytek.springclouddemo.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Zuul 网关
+ * 2019/10/3 21:21
+ *
+ * @author DuHuang
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
