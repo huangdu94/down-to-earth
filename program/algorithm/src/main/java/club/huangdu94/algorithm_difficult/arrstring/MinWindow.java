dfee7e53496e1863db978cedfbf907ea70d955d9
huangdu94 <tiancaihuangdu@gmail.com>
2020-07-27 20:32:30 +0800

    最大盛水。

---
+++ program/algorithm/src/main/java/club/huangdu94/algorithm_difficult/arrstring/MinWindow.java
@@ -0,0 +1,20 @@
+package club.huangdu94.algorithm_difficult.arrstring;
+
+/**
+ * 最小覆盖子串
+ * 给你一个字符串 S、一个字符串 T，请在字符串 S 里面找出：包含 T 所有字符的最小子串。
+ * 示例：
+ * 输入: S = "ADOBECODEBANC", T = "ABC"
+ * 输出: "BANC"
+ * 说明：
+ * 如果 S 中不存这样的子串，则返回空字符串 ""。
+ * 如果 S 中存在这样的子串，我们保证它是唯一的答案。
+ *
+ * @author duhuang@iflytek.com
+ * @version 2020/7/27 20:29
+ */
+public class MinWindow {
+    public String minWindow(String s, String t) {
+        return null;
+    }
+}
