26dedcb0d2354d6d145df204939b7591f752d2d0
huangdu94 <tiancaihuangdu@gmail.com>
2020-08-03 13:32:44 +0800

    滑动窗口最大值。

--- program/algorithm/src/main/java/club/huangdu94/algorithm_difficult/arrstring/MaxSlidingWindow.java
+++ program/algorithm/src/main/java/club/huangdu94/algorithm_difficult/arrstring/MaxSlidingWindow.java
@@ -1,5 +1,7 @@
 package club.huangdu94.algorithm_difficult.arrstring;
 
+import java.util.Arrays;
+
 /**
  * 滑动窗口最大值
  * 给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
@@ -27,7 +29,46 @@ package club.huangdu94.algorithm_difficult.arrstring;
  * @version 2020/7/27 20:27
  */
 public class MaxSlidingWindow {
+    // 时间复杂度依然不是线性，但相比暴力解大大优化
+    //最好的情况下时间复杂度为o(n-k) 最坏的情况下o(n*k-k^2)
     public int[] maxSlidingWindow(int[] nums, int k) {
-        return null;
+        int len = nums.length - k + 1;
+        int[] result = new int[len];
+        int max = this.findMax(nums, 0, k - 1);
+        result[0] = max;
+        for (int i = 1; i < len; i++) {
+            // 新来的比原max大或等于，直接更新max
+            if (max <= nums[i + k - 1])
+                max = nums[i + k - 1];
+                // 新来的比原来的小，但原最大的不是消失的则max不变
+                // 新来的比原来的小，但原最大的是消失的则max需要被重新找
+            else if (max == nums[i - 1])
+                max = this.findMax(nums, i, i + k - 1);
+            result[i] = max;
+        }
+        return result;
+    }
+
+    // 暴力解 时间复杂度o(n*k-k^2)
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        int len = nums.length - k + 1;
+        int[] result = new int[len];
+        for (int i = 0; i < len; i++)
+            result[i] = this.findMax(nums, i, i + k - 1);
+        return result;
+    }
+
+    private int findMax(int[] nums, int l, int r) {
+        int max = nums[l];
+        for (int i = l + 1; i <= r; i++)
+            if (max < nums[i])
+                max = nums[i];
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1};
+        MaxSlidingWindow maxSlidingWindow = new MaxSlidingWindow();
+        System.out.println(Arrays.toString(maxSlidingWindow.maxSlidingWindow(nums, 1)));
     }
 }
