a4bbed530aed2970ced3393824eae1bcd5f17bba
huangdu94 <tiancaihuangdu@gmail.com>
2020-08-06 15:59:27 +0800

    超时。

--- program/algorithm/src/main/java/club/huangdu94/algorithm_difficult/treeGraph/LadderLength.java
+++ program/algorithm/src/main/java/club/huangdu94/algorithm_difficult/treeGraph/LadderLength.java
@@ -1,9 +1,6 @@
 package club.huangdu94.algorithm_difficult.treeGraph;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * 单词接龙
@@ -40,7 +37,16 @@ public class LadderLength {
     private int result = 0;
 
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        this.transform(1, beginWord, endWord, wordList);
+        // 如果wordList中根本就没有beginWord则不可能转换，无需递归
+        boolean flag = false;
+        for (String word : wordList)
+            if (word.equals(endWord)) {
+                flag = true;
+                break;
+            }
+        // 设置一个黑名单用于提前剪枝
+        Set<String> blackSet = new HashSet<>();
+        if (flag) this.transform(1, beginWord, endWord, wordList, blackSet);
         return result;
     }
 
@@ -51,46 +57,56 @@ public class LadderLength {
      * @param curWord        当前单词
      * @param endWord        目标单词
      * @param remainWordList 当前遗留单词列表
+     * @param blackSet       黑名单
+     * @return false表示该分支查找失败，单词加入黑名单下次遇到直接剪枝
      */
-    private void transform(int pathLen, String curWord, String endWord, List<String> remainWordList) {
+    private boolean transform(int pathLen, String curWord, String endWord, List<String> remainWordList, Set<String> blackSet) {
         // 0. 路径长度加一
         pathLen += 1;
-        // 1. 寻找当前单词可以转换的单词列表
-        List<String> filtered = this.getFilterList(curWord, remainWordList);
-        // 2. 查看此过滤后的单词列表是否包含目标的endWord，如果包含则记录result值，此分支递归结束
+        // 1. 提前剪枝，抛弃没有意义的分支
+        if (blackSet.contains(curWord)) return false;
+        if (result != 0 && result <= pathLen) return true;
+        // 2. 寻找当前单词可以转换的单词列表
+        List<String> filtered = new ArrayList<>();
+        // 3. 查看此过滤后的单词列表是否包含目标的endWord，如果包含则记录result值，此分支递归结束
+        if (this.getFilterList(curWord, endWord, remainWordList, filtered)) {
+            if (result == 0 || pathLen < result)
+                result = pathLen;
+            // 该轮递归结束，恢复wordList为进入该层时的状态
+            remainWordList.addAll(filtered);
+            return true;
+        }
+        // 4. 不包含的话，则递归每一个过滤后单词列表
+        boolean result = false;
         for (String word : filtered)
-            if (word.equals(endWord)) {
-                if (result == 0 || pathLen < result)
-                    result = pathLen;
-                // 该轮递归结束，恢复wordList为进入该层时的状态
-                remainWordList.addAll(filtered);
-                return;
-            }
-        // 3. 不包含的话，则递归每一个过滤后单词列表
-        for (String word : filtered)
-            this.transform(pathLen, word, endWord, remainWordList);
-        // 4. 该轮递归结束，恢复wordList为进入该层时的状态
+            if (!this.transform(pathLen, word, endWord, remainWordList, blackSet)) blackSet.add(word);
+            else result = true;
+        // 5. 该轮递归结束，恢复wordList为进入该层时的状态
         remainWordList.addAll(filtered);
+        return result;
     }
 
     /**
      * 从list中找到和word仅有一个字母不相同的字母并返回新的list
      *
      * @param curWord        当前单词
+     * @param endWord        结束单词
      * @param remainWordList 剩余单词列表
-     * @return 满足过滤条件的list
+     * @param filtered       满足过滤条件的list
+     * @return 是否找到endWord，true的话表示找到
      */
-    private List<String> getFilterList(String curWord, List<String> remainWordList) {
-        List<String> filtered = new ArrayList<>();
+    private boolean getFilterList(String curWord, String endWord, List<String> remainWordList, List<String> filtered) {
         Iterator<String> iterator = remainWordList.iterator();
         while (iterator.hasNext()) {
             String target = iterator.next();
             if (this.filter(curWord, target)) {
+                if (target.equals(endWord))
+                    return true;
                 filtered.add(target);
                 iterator.remove();
             }
         }
-        return filtered;
+        return false;
     }
 
     /**
@@ -98,7 +114,7 @@ public class LadderLength {
      *
      * @param curWord 当前单词
      * @param target  目标单词
-     * @return 查看当前单词和目标单词是否只有一个字母不一样
+     * @return 是否满足过滤条件(只有一个字母不一样则满足条件)
      */
     private boolean filter(String curWord, String target) {
         int count = 0;
@@ -111,9 +127,9 @@ public class LadderLength {
 
     public static void main(String[] args) {
         LadderLength ladderLength = new LadderLength();
-        String beginWord = "hit";
-        String endWord = "cog";
-        List<String> wordList = new ArrayList<>(Arrays.asList("hot", "dot", "dog", "lot", "log", "cog"));
+        String beginWord = "cet";
+        String endWord = "ism";
+        List<String> wordList = new ArrayList<>(Arrays.asList("kid", "tag", "pup", "ail", "tun", "woo", "erg", "luz", "brr", "gay", "sip", "kay", "per", "val", "mes", "ohs", "now", "boa", "cet", "pal", "bar", "die", "war", "hay", "eco", "pub", "lob", "rue", "fry", "lit", "rex", "jan", "cot", "bid", "ali", "pay", "col", "gum", "ger", "row", "won", "dan", "rum", "fad", "tut", "sag", "yip", "sui", "ark", "has", "zip", "fez", "own", "ump", "dis", "ads", "max", "jaw", "out", "btu", "ana", "gap", "cry", "led", "abe", "box", "ore", "pig", "fie", "toy", "fat", "cal", "lie", "noh", "sew", "ono", "tam", "flu", "mgm", "ply", "awe", "pry", "tit", "tie", "yet", "too", "tax", "jim", "san", "pan", "map", "ski", "ova", "wed", "non", "wac", "nut", "why", "bye", "lye", "oct", "old", "fin", "feb", "chi", "sap", "owl", "log", "tod", "dot", "bow", "fob", "for", "joe", "ivy", "fan", "age", "fax", "hip", "jib", "mel", "hus", "sob", "ifs", "tab", "ara", "dab", "jag", "jar", "arm", "lot", "tom", "sax", "tex", "yum", "pei", "wen", "wry", "ire", "irk", "far", "mew", "wit", "doe", "gas", "rte", "ian", "pot", "ask", "wag", "hag", "amy", "nag", "ron", "soy", "gin", "don", "tug", "fay", "vic", "boo", "nam", "ave", "buy", "sop", "but", "orb", "fen", "paw", "his", "sub", "bob", "yea", "oft", "inn", "rod", "yam", "pew", "web", "hod", "hun", "gyp", "wei", "wis", "rob", "gad", "pie", "mon", "dog", "bib", "rub", "ere", "dig", "era", "cat", "fox", "bee", "mod", "day", "apr", "vie", "nev", "jam", "pam", "new", "aye", "ani", "and", "ibm", "yap", "can", "pyx", "tar", "kin", "fog", "hum", "pip", "cup", "dye", "lyx", "jog", "nun", "par", "wan", "fey", "bus", "oak", "bad", "ats", "set", "qom", "vat", "eat", "pus", "rev", "axe", "ion", "six", "ila", "lao", "mom", "mas", "pro", "few", "opt", "poe", "art", "ash", "oar", "cap", "lop", "may", "shy", "rid", "bat", "sum", "rim", "fee", "bmw", "sky", "maj", "hue", "thy", "ava", "rap", "den", "fla", "auk", "cox", "ibo", "hey", "saw", "vim", "sec", "ltd", "you", "its", "tat", "dew", "eva", "tog", "ram", "let", "see", "zit", "maw", "nix", "ate", "gig", "rep", "owe", "ind", "hog", "eve", "sam", "zoo", "any", "dow", "cod", "bed", "vet", "ham", "sis", "hex", "via", "fir", "nod", "mao", "aug", "mum", "hoe", "bah", "hal", "keg", "hew", "zed", "tow", "gog", "ass", "dem", "who", "bet", "gos", "son", "ear", "spy", "kit", "boy", "due", "sen", "oaf", "mix", "hep", "fur", "ada", "bin", "nil", "mia", "ewe", "hit", "fix", "sad", "rib", "eye", "hop", "haw", "wax", "mid", "tad", "ken", "wad", "rye", "pap", "bog", "gut", "ito", "woe", "our", "ado", "sin", "mad", "ray", "hon", "roy", "dip", "hen", "iva", "lug", "asp", "hui", "yak", "bay", "poi", "yep", "bun", "try", "lad", "elm", "nat", "wyo", "gym", "dug", "toe", "dee", "wig", "sly", "rip", "geo", "cog", "pas", "zen", "odd", "nan", "lay", "pod", "fit", "hem", "joy", "bum", "rio", "yon", "dec", "leg", "put", "sue", "dim", "pet", "yaw", "nub", "bit", "bur", "sid", "sun", "oil", "red", "doc", "moe", "caw", "eel", "dix", "cub", "end", "gem", "off", "yew", "hug", "pop", "tub", "sgt", "lid", "pun", "ton", "sol", "din", "yup", "jab", "pea", "bug", "gag", "mil", "jig", "hub", "low", "did", "tin", "get", "gte", "sox", "lei", "mig", "fig", "lon", "use", "ban", "flo", "nov", "jut", "bag", "mir", "sty", "lap", "two", "ins", "con", "ant", "net", "tux", "ode", "stu", "mug", "cad", "nap", "gun", "fop", "tot", "sow", "sal", "sic", "ted", "wot", "del", "imp", "cob", "way", "ann", "tan", "mci", "job", "wet", "ism", "err", "him", "all", "pad", "hah", "hie", "aim", "ike", "jed", "ego", "mac", "baa", "min", "com", "ill", "was", "cab", "ago", "ina", "big", "ilk", "gal", "tap", "duh", "ola", "ran", "lab", "top", "gob", "hot", "ora", "tia", "kip", "han", "met", "hut", "she", "sac", "fed", "goo", "tee", "ell", "not", "act", "gil", "rut", "ala", "ape", "rig", "cid", "god", "duo", "lin", "aid", "gel", "awl", "lag", "elf", "liz", "ref", "aha", "fib", "oho", "tho", "her", "nor", "ace", "adz", "fun", "ned", "coo", "win", "tao", "coy", "van", "man", "pit", "guy", "foe", "hid", "mai", "sup", "jay", "hob", "mow", "jot", "are", "pol", "arc", "lax", "aft", "alb", "len", "air", "pug", "pox", "vow", "got", "meg", "zoe", "amp", "ale", "bud", "gee", "pin", "dun", "pat", "ten", "mob"));
         int r = ladderLength.ladderLength(beginWord, endWord, wordList);
         System.out.println(r);
     }
