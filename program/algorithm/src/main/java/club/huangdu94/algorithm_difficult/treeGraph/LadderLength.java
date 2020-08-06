0ad5588b9157c9ed0e605f53c0a6b4f73277f261
huangdu94 <tiancaihuangdu@gmail.com>
2020-08-06 14:40:49 +0800

    通过一半 然后超时。

--- program/algorithm/src/main/java/club/huangdu94/algorithm_difficult/treeGraph/LadderLength.java
+++ program/algorithm/src/main/java/club/huangdu94/algorithm_difficult/treeGraph/LadderLength.java
@@ -1,5 +1,8 @@
 package club.huangdu94.algorithm_difficult.treeGraph;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -33,7 +36,85 @@ import java.util.List;
  * @version 2020/8/4 12:06
  */
 public class LadderLength {
+    // 结果
+    private int result = 0;
+
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        return -1;
+        this.transform(1, beginWord, endWord, wordList);
+        return result;
+    }
+
+    /**
+     * 递归寻找最短转换路径
+     *
+     * @param pathLen        当前转换路径长度
+     * @param curWord        当前单词
+     * @param endWord        目标单词
+     * @param remainWordList 当前遗留单词列表
+     */
+    private void transform(int pathLen, String curWord, String endWord, List<String> remainWordList) {
+        // 0. 路径长度加一
+        pathLen += 1;
+        // 1. 寻找当前单词可以转换的单词列表
+        List<String> filtered = this.getFilterList(curWord, remainWordList);
+        // 2. 查看此过滤后的单词列表是否包含目标的endWord，如果包含则记录result值，此分支递归结束
+        for (String word : filtered)
+            if (word.equals(endWord)) {
+                if (result == 0 || pathLen < result)
+                    result = pathLen;
+                // 该轮递归结束，恢复wordList为进入该层时的状态
+                remainWordList.addAll(filtered);
+                return;
+            }
+        // 3. 不包含的话，则递归每一个过滤后单词列表
+        for (String word : filtered)
+            this.transform(pathLen, word, endWord, remainWordList);
+        // 4. 该轮递归结束，恢复wordList为进入该层时的状态
+        remainWordList.addAll(filtered);
+    }
+
+    /**
+     * 从list中找到和word仅有一个字母不相同的字母并返回新的list
+     *
+     * @param curWord        当前单词
+     * @param remainWordList 剩余单词列表
+     * @return 满足过滤条件的list
+     */
+    private List<String> getFilterList(String curWord, List<String> remainWordList) {
+        List<String> filtered = new ArrayList<>();
+        Iterator<String> iterator = remainWordList.iterator();
+        while (iterator.hasNext()) {
+            String target = iterator.next();
+            if (this.filter(curWord, target)) {
+                filtered.add(target);
+                iterator.remove();
+            }
+        }
+        return filtered;
+    }
+
+    /**
+     * 过滤器
+     *
+     * @param curWord 当前单词
+     * @param target  目标单词
+     * @return 查看当前单词和目标单词是否只有一个字母不一样
+     */
+    private boolean filter(String curWord, String target) {
+        int count = 0;
+        for (int i = 0; i < curWord.length(); i++)
+            if (curWord.charAt(i) != target.charAt(i))
+                if (++count > 1)
+                    return false;
+        return count == 1;
+    }
+
+    public static void main(String[] args) {
+        LadderLength ladderLength = new LadderLength();
+        String beginWord = "hit";
+        String endWord = "cog";
+        List<String> wordList = new ArrayList<>(Arrays.asList("hot", "dot", "dog", "lot", "log", "cog"));
+        int r = ladderLength.ladderLength(beginWord, endWord, wordList);
+        System.out.println(r);
     }
 }
