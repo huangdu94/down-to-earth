dc154c9f35ab1237933d14ad34e2b9375a5bc3ef
huangdu94 <tiancaihuangdu@gmail.com>
2020-08-09 10:50:20 +0800

    记录两个问题。

---
+++ program/algorithm/src/main/java/club/huangdu94/algorithm_difficult/treeGraph/MaxPathSum.java
@@ -0,0 +1,29 @@
+package club.huangdu94.algorithm_difficult.treeGraph;
+
+/**
+ * 二叉树中的最大路径和
+ * 给定一个非空二叉树，返回其最大路径和。
+ * 本题中，路径被定义为一条从树中任意节点出发，达到任意节点的序列。该路径至少包含一个节点，且不一定经过根节点。
+ * 示例 1:
+ * 输入: [1,2,3]
+ * *       1
+ * *      / \
+ * *     2   3
+ * 输出: 6
+ * 示例2:
+ * 输入: [-10,9,20,null,null,15,7]
+ * *  -10
+ * * / \
+ * * 9 20
+ * *  / \
+ * * 15  7
+ * 输出: 42
+ *
+ * @author duhuang@iflytek.com
+ * @version 2020/8/9 10:29
+ */
+public class MaxPathSum {
+    public int maxPathSum(TreeNode root) {
+        return 0;
+    }
+}
