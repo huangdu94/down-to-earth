c840caa1548b33231a00310f73afa8a437892686
huangdu94 <tiancaihuangdu@gmail.com>
2020-08-15 15:06:40 +0800

    二叉搜索树的最近公共祖先。

--- program/algorithms/src/main/java/club/huangdu94/exploration/advanced_algorithms/treeGraph/LowestCommonAncestor.java
+++ program/algorithms/src/main/java/club/huangdu94/exploration/advanced_algorithms/treeGraph/LowestCommonAncestor.java
@@ -3,7 +3,7 @@ package club.huangdu94.exploration.advanced_algorithms.treeGraph;
 import club.huangdu94.data_structure.TreeNode;
 
 /**
- * 235. 二叉搜索树的最近公共祖先
+ * 236. 二叉树的最近公共祖先
  * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
  * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
  * 例如，给定如下二叉树: root =[3,5,1,6,2,0,8,null,null,7,4]
@@ -21,6 +21,7 @@ import club.huangdu94.data_structure.TreeNode;
  *
  * @author duhuang@iflytek.com
  * @version 2020/8/6 16:00
+ * @see club.huangdu94.question_bank.easy.LowestCommonAncestor
  */
 public class LowestCommonAncestor {
 
