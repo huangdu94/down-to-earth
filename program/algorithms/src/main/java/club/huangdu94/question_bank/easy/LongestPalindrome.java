66359791fec090b617bd8979e50115f1e3c34b6d
huangdu94 <tiancaihuangdu@gmail.com>
2020-08-20 17:36:38 +0800

    最长回文串

---
+++ program/algorithms/src/main/java/club/huangdu94/question_bank/easy/LongestPalindrome.java
@@ -0,0 +1,48 @@
+package club.huangdu94.question_bank.easy;
+
+/**
+ * 409. 最长回文串
+ * 给定一个包含大写字母和小写字母的字符串，找到通过这些字母构造成的最长的回文串。
+ * 在构造过程中，请注意区分大小写。比如 "Aa" 不能当做一个回文字符串。
+ * 注意:
+ * 假设字符串的长度不会超过 1010。
+ * 示例 1:
+ * 输入:
+ * "abccccdd"
+ * 输出:
+ * 7
+ * 解释:
+ * 我们可以构造的最长的回文串是"dccaccd", 它的长度是 7。
+ *
+ * @author duhuang@iflytek.com
+ * @version 2020/8/20 17:19
+ */
+public class LongestPalindrome {
+    public int longestPalindrome(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int[] counts = new int[52];
+        for (int i = 0; i < s.length(); i++) {
+            counts[getMapping(s.charAt(i))]++;
+        }
+        int res = 0;
+        // 是否可以加1
+        boolean flag = false;
+        for (int c : counts) {
+            if (c % 2 == 0)
+                res += c;
+            else {
+                res += (c - 1);
+                if (!flag) flag = true;
+            }
+        }
+        return flag ? res + 1 : res;
+    }
+
+    /**
+     * A-Z -> 0-25
+     * a-z -> 26-51
+     */
+    private int getMapping(char ch) {
+        return ch >= 'a' ? ch - 72 : ch - 65;
+    }
+}
