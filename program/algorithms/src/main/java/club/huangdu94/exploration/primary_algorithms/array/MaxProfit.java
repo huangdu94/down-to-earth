7937612f7b9b9d20e7134e5c622ff53729ee7d19
huangdu94 <tiancaihuangdu@gmail.com>
2020-08-26 21:23:59 +0800

    买卖股票一二。

--- program/algorithms/src/main/java/club/huangdu94/exploration/primary_algorithms/array/MaxProfit.java
+++ program/algorithms/src/main/java/club/huangdu94/exploration/primary_algorithms/array/MaxProfit.java
@@ -3,6 +3,29 @@ package club.huangdu94.exploration.primary_algorithms.array;
 import java.util.HashSet;
 
 /**
+ * 买卖股票的最佳时机 II
+ * 给定一个数组，它的第i 个元素是一支给定股票第 i 天的价格。
+ * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * 示例 1:
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 7
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ * 随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+ * 示例 2:
+ * 输入: [1,2,3,4,5]
+ * 输出: 4
+ * 解释: 在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ * 注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。
+ * 因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+ * 示例3:
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ * 提示：
+ * 1 <= prices.length <= 3 * 10 ^ 4
+ * 0 <= prices[i]<= 10 ^ 4
+ *
  * @author duhuang@iflytek.com
  * @version 2020/7/26 15:08
  */
@@ -25,7 +48,7 @@ public class MaxProfit {
         return calculate(prices, 0);
     }
 
-    public int calculate(int prices[], int s) {
+    public int calculate(int[] prices, int s) {
         new HashSet<>();
         // s表示当前开始计算的index,如果s>=prices.length,直接返回0
         if (s >= prices.length)
@@ -49,5 +72,23 @@ public class MaxProfit {
                 max = maxprofit;
         }
         return max;
+    }
+
+    // 动态规划
+    public int maxProfit3(int[] prices) {
+        int len = prices.length;
+        if (len == 0)
+            return 0;
+        int[][] dp = new int[len][2];
+        // 第一天
+        // 持有现金
+        dp[0][0] = 0;
+        // 持有股票
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][0] - prices[i], dp[i - 1][1]);
+        }
+        return dp[len - 1][0];
     }
 }
