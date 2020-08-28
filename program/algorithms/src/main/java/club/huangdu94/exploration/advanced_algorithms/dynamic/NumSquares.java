62c1824bc95ea33d254bfe587ebe8bf22a8a1536
huangdu94 <tiancaihuangdu@gmail.com>
2020-08-28 16:45:51 +0800

    更新

--- program/algorithms/src/main/java/club/huangdu94/exploration/advanced_algorithms/dynamic/NumSquares.java
+++ program/algorithms/src/main/java/club/huangdu94/exploration/advanced_algorithms/dynamic/NumSquares.java
@@ -1,8 +1,22 @@
 package club.huangdu94.exploration.advanced_algorithms.dynamic;
 
 /**
+ * 完全平方数
+ * 给定正整数n，找到若干个完全平方数（比如1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+ * 示例1:
+ * 输入: n = 12
+ * 输出: 3
+ * 解释: 12 = 4 + 4 + 4.
+ * 示例 2:
+ * 输入: n = 13
+ * 输出: 2
+ * 解释: 13 = 4 + 9.
+ *
  * @author duhuang@iflytek.com
  * @version 2020/8/28 12:37
  */
 public class NumSquares {
+    public int numSquares(int n) {
+        return -1;
+    }
 }
