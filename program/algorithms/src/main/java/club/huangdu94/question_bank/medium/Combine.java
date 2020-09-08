ec70e269981f3b7f9235f332004bd4ae9eca9561
huangdu94 <tiancaihuangdu@gmail.com>
2020-09-08 11:37:08 +0800

    组合。

--- program/algorithms/src/main/java/club/huangdu94/question_bank/medium/Combine.java
+++ program/algorithms/src/main/java/club/huangdu94/question_bank/medium/Combine.java
@@ -54,3 +54,40 @@ public class Combine {
         }
     }
 }
+
+class Solution {
+    List<Integer> temp = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer> temp = new ArrayList<Integer>();
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        // 初始化
+        // 将 temp 中 [0, k - 1] 每个位置 i 设置为 i + 1，即 [0, k - 1] 存 [1, k]
+        // 末尾加一位 n + 1 作为哨兵
+        for (int i = 1; i <= k; ++i) {
+            temp.add(i);
+        }
+        temp.add(n + 1);
+
+        int j = 0;
+        while (j < k) {
+            ans.add(new ArrayList<Integer>(temp.subList(0, k)));
+            j = 0;
+            // 寻找第一个 temp[j] + 1 != temp[j + 1] 的位置 t
+            // 我们需要把 [0, t - 1] 区间内的每个位置重置成 [1, t]
+            while (j < k && temp.get(j) + 1 == temp.get(j + 1)) {
+                temp.set(j, j + 1);
+                ++j;
+            }
+            // j 是第一个 temp[j] + 1 != temp[j + 1] 的位置
+            temp.set(j, temp.get(j) + 1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.combine(5, 3));
+    }
+}
