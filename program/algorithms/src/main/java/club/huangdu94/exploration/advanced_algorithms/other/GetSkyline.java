7929d87a0f3dd0412644b64545c13ffeee203bd8
huangdu94 <tiancaihuangdu@gmail.com>
2020-09-13 11:39:58 +0800

    218. 天际线问题

--- program/algorithms/src/main/java/club/huangdu94/exploration/advanced_algorithms/other/GetSkyline.java
+++ program/algorithms/src/main/java/club/huangdu94/exploration/advanced_algorithms/other/GetSkyline.java
@@ -3,7 +3,7 @@ package club.huangdu94.exploration.advanced_algorithms.other;
 import java.util.*;
 
 /**
- * 天际线问题
+ * 218. 天际线问题
  * 城市的天际线是从远处观看该城市中所有建筑物形成的轮廓的外部轮廓。现在，假设您获得了城市风光照片（图A）上显示的所有建筑物的位置和高度，请编写一个程序以输出由这些建筑物形成的天际线（图B）。
  * 每个建筑物的几何信息用三元组[Li，Ri，Hi] 表示，其中 Li 和 Ri 分别是第 i 座建筑物左右边缘的 x 坐标，Hi 是其高度。可以保证0 ≤ Li, Ri ≤ INT_MAX,0 < Hi ≤ INT_MAX 和 Ri - Li > 0。您可以假设所有建筑物都是在绝对平坦且高度为 0 的表面上的完美矩形。
  * 例如，图A中所有建筑物的尺寸记录为：[ [2 9 10], [3 7 15], [5 12 12], [15 20 10], [19 24 8] ] 。
@@ -19,6 +19,9 @@ import java.util.*;
  * @version 2020/9/8 12:44
  */
 public class GetSkyline {
+    /**
+     * heightList里存放着每一段区间里的高度，长度为3的一维数组存放[x,y,h]
+     */
     private LinkedList<int[]> heightList;
 
     public List<List<Integer>> getSkyline(int[][] buildings) {
@@ -26,52 +29,59 @@ public class GetSkyline {
         int len = buildings.length;
         if (len == 0) return new ArrayList<>();
         for (int[] building : buildings) add(building);
+        // 将heightList处理成最后的结果，每一个线段取左端点（如果和上一个线段处于同一高度则跳过）
+        // 最后按照题意添加最右端点
         List<List<Integer>> res = new ArrayList<>(heightList.size() + 1);
-        int pre = -1;
-        for (int[] element : heightList) {
-            if (pre == element[2]) continue;
-            pre = element[2];
-            res.add(Arrays.asList(element[0], element[2]));
+        int curHeight = 0;
+        for (int[] h : heightList) {
+            if (curHeight == h[2]) continue;
+            curHeight = h[2];
+            res.add(Arrays.asList(h[0], h[2]));
         }
         res.add(Arrays.asList(heightList.getLast()[1], 0));
         return res;
     }
 
     private void add(int[] building) {
+        // 添加第一个新building，不存在合并所以直接加该building的左上点和右下点
         if (heightList.isEmpty()) {
             heightList.add(building);
-        } else {
-            ListIterator<int[]> iterator = heightList.listIterator(heightList.size());
-            while (iterator.hasPrevious()) {
-                if (building[0] >= iterator.previous()[1]) {
-                    iterator.next();
-                    break;
+            return;
+        }
+        // 获得heightList链表最后位置迭代器，从后向前遍历
+        ListIterator<int[]> iterator = heightList.listIterator(heightList.size());
+        // 找到用于合并的第一个位置，如果某一个区间的y已经大于building的x，说明迭代器已经已经错过了需要合并的区间，迭代器回头
+        while (iterator.hasPrevious()) {
+            if (building[0] >= iterator.previous()[1]) {
+                iterator.next();
+                break;
+            }
+        }
+        // 开始合并
+        while (iterator.hasNext()) {
+            int[] next = iterator.next();
+            // 只有当building的高度大于现有高度时候才需要合并
+            if (building[2] > next[2]) {
+                iterator.remove();
+                if (building[0] != next[0]) {
+                    iterator.add(new int[]{next[0], building[0], next[2]});
+                }
+                if (building[1] < next[1]) {
+                    iterator.add(Arrays.copyOf(building, 3));
+                    iterator.add(new int[]{building[1], next[1], next[2]});
+                } else {
+                    iterator.add(new int[]{building[0], next[1], building[2]});
                 }
             }
-            while (iterator.hasNext()) {
-                int[] next = iterator.next();
-                if (building[2] > next[2]) {
-                    iterator.remove();
-                    if (building[0] != next[0]) {
-                        iterator.add(new int[]{next[0], building[0], next[2]});
-                    }
-                    if (building[1] < next[1]) {
-                        iterator.add(Arrays.copyOf(building, 3));
-                        iterator.add(new int[]{building[1], next[1], next[2]});
-                    } else {
-                        iterator.add(new int[]{building[0], next[1], building[2]});
-                    }
-                }
-                building[0] = next[1];
-                if (building[1] <= next[1]) break;
+            building[0] = next[1];
+            if (building[1] <= next[1]) break;
+        }
+        if (!iterator.hasNext() && building[0] < building[1]) {
+            int[] tail = heightList.getLast();
+            if (tail[1] < building[0]) {
+                heightList.add(new int[]{tail[1], building[0], 0});
             }
-            if (!iterator.hasNext() && building[0] < building[1]) {
-                int[] tail = heightList.getLast();
-                if (tail[1] < building[0]) {
-                    heightList.add(new int[]{tail[1], building[0], 0});
-                }
-                heightList.add(building);
-            }
+            heightList.add(building);
         }
     }
 
