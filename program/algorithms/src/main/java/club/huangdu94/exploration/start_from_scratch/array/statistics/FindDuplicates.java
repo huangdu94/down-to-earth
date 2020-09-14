3e92ff1d1288c3afa1f7506725f0f08bfd8b3395
huangdu94 <tiancaihuangdu@gmail.com>
2020-09-14 16:53:47 +0800

    四道题。

---
+++ program/algorithms/src/main/java/club/huangdu94/exploration/start_from_scratch/array/statistics/FindDuplicates.java
@@ -0,0 +1,58 @@
+package club.huangdu94.exploration.start_from_scratch.array.statistics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 442. 数组中重复的数据
+ * 给定一个整数数组 a，其中1 ≤ a[i] ≤ n （n为数组长度）, 其中有些元素出现两次而其他元素出现一次。
+ * 找到所有出现两次的元素。
+ * 你可以不用到任何额外空间并在O(n)时间复杂度内解决这个问题吗？
+ * 示例：
+ * 输入:
+ * [4,3,2,7,8,2,3,1]
+ * 输出:
+ * [2,3]
+ *
+ * @author duhuang@iflytek.com
+ * @version 2020/9/14 14:30
+ */
+public class FindDuplicates {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                res.add(nums[i]);
+            }
+        }
+        return res;
+    }
+
+    public List<Integer> findDuplicates2(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                res.add(index + 1);
+            } else {
+                nums[index] *= -1;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        FindDuplicates findDuplicates = new FindDuplicates();
+        System.out.println(findDuplicates.findDuplicates(nums));
+    }
+}
