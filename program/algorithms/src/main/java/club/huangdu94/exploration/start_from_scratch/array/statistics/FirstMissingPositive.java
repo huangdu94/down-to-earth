93dcf9455facd8bc27cd0d37f2b2065e34467eca
huangdu94 <tiancaihuangdu@gmail.com>
2020-09-14 11:59:55 +0800

    41. 缺失的第一个正数

---
+++ program/algorithms/src/main/java/club/huangdu94/exploration/start_from_scratch/array/statistics/FirstMissingPositive.java
@@ -0,0 +1,23 @@
+package club.huangdu94.exploration.start_from_scratch.array.statistics;
+
+/**
+ * 41. 缺失的第一个正数
+ * 给你一个未排序的整数数组，请你找出其中没有出现的最小的正整数。
+ * 示例 1:
+ * 输入: [1,2,0]
+ * 输出: 3
+ * 示例 2:
+ * 输入: [3,4,-1,1]
+ * 输出: 2
+ * 示例 3:
+ * 输入: [7,8,9,11,12]
+ * 输出: 1
+ * 提示：
+ * 你的算法的时间复杂度应为O(n)，并且只能使用常数级别的额外空间。
+ *
+ * @author duhuang@iflytek.com
+ * @version 2020/9/14 10:55
+ * @see club.huangdu94.exploration.advanced_algorithms.array_string.FirstMissingPositive
+ */
+public class FirstMissingPositive {
+}
