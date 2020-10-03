379bb965b2d506d91a2f2cfb86c4d5113c05e25a
huangdu94 <tiancaihuangdu@gmail.com>
2020-10-03 11:46:57 +0800

    字符串转换两题。

--- program/algorithms/src/main/java/club/huangdu94/exploration/primary_algorithms/array/TwoSum.java
+++ program/algorithms/src/main/java/club/huangdu94/exploration/primary_algorithms/array/TwoSum.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 两数之和
+ * 1. 两数之和
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
  * 示例:
@@ -40,5 +40,17 @@ public class TwoSum {
             }
         }
         return result;
+    }
+
+    public int[] twoSum3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0, n = nums.length; i < n; i++) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            } else {
+                map.put(target - nums[i], i);
+            }
+        }
+        return null;
     }
 }
