b9e26034d380a9688308ca0732df0a99c87961fc
huangdu94 <tiancaihuangdu@gmail.com>
2020-10-04 09:35:13 +0800

    2. 两数相加

--- program/algorithms/src/main/java/club/huangdu94/exploration/intermediate_algorithms/linkedlist/AddTwoNumbers.java
+++ program/algorithms/src/main/java/club/huangdu94/exploration/intermediate_algorithms/linkedlist/AddTwoNumbers.java
@@ -3,7 +3,7 @@ package club.huangdu94.exploration.intermediate_algorithms.linkedlist;
 import club.huangdu94.data_structure.ListNode;
 
 /**
- * 两数相加
+ * 2. 两数相加
  * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
  * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
  * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
@@ -49,5 +49,19 @@ public class AddTwoNumbers {
         else if (flag)
             p0.next = new ListNode(1);
         return dummy.next;
+    }
+
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode(), cur = res;
+        int add = 0;
+        while (l1 != null || l2 != null || add != 0) {
+            int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + add;
+            cur.next = new ListNode(sum % 10);
+            add = sum / 10;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+            cur = cur.next;
+        }
+        return res.next;
     }
 }
