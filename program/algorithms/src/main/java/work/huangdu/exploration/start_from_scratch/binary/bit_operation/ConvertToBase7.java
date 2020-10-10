00a1f6431316fe9d34448aa9cbde732f6e6ed0b3
huangdu94 <tiancaihuangdu@gmail.com>
2020-10-10 16:15:33 +0800

    504. 七进制数

---
+++ program/algorithms/src/main/java/work/huangdu/exploration/start_from_scratch/binary/bit_operation/ConvertToBase7.java
@@ -0,0 +1,21 @@
+package work.huangdu.exploration.start_from_scratch.binary.bit_operation;
+
+/**
+ * 504. 七进制数
+ * 给定一个整数，将其转化为7进制，并以字符串形式输出。
+ * 示例 1:
+ * 输入: 100
+ * 输出: "202"
+ * 示例 2:
+ * 输入: -7
+ * 输出: "-10"
+ * 注意: 输入范围是 [-1e7, 1e7] 。
+ *
+ * @author duhuang@iflytek.com
+ * @version 2020/10/10 16:05
+ */
+public class ConvertToBase7 {
+    public String convertToBase7(int num) {
+        return Integer.toString(num, 7);
+    }
+}
