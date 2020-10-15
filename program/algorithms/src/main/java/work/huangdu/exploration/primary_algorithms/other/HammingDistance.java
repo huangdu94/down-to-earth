7689f92b67d6a025ffdca49236738238950f2081
huangdu94 <tiancaihuangdu@gmail.com>
2020-10-15 13:15:20 +0800

    汉明距离

--- program/algorithms/src/main/java/work/huangdu/exploration/primary_algorithms/other/HammingDistance.java
+++ program/algorithms/src/main/java/work/huangdu/exploration/primary_algorithms/other/HammingDistance.java
@@ -1,7 +1,7 @@
 package work.huangdu.exploration.primary_algorithms.other;
 
 /**
- * 汉明距离
+ * 461. 汉明距离
  * 两个整数之间的汉明距离指的是这两个数字对应二进制位不同的位置的数目。
  * 给出两个整数 x 和 y，计算它们之间的汉明距离。
  * 注意：
---
+++ program/algorithms/src/main/java/work/huangdu/exploration/start_from_scratch/binary/bit_operation/HammingDistance.java
@@ -0,0 +1,9 @@
+package work.huangdu.exploration.start_from_scratch.binary.bit_operation;
+
+/**
+ * @author duhuang@iflytek.com
+ * @version 2020/10/15 12:33
+ * @see work.huangdu.exploration.primary_algorithms.other.HammingDistance
+ */
+public class HammingDistance {
+}
