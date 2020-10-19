df9224caffd43c0d8646bbd9e032f9c46a170876
huangdu94 <tiancaihuangdu@gmail.com>
2020-10-19 16:10:56 +0800

    三题。

---
+++ program/algorithms/src/main/java/work/huangdu/exploration/start_from_scratch/binary/number_bit_operation/CountNumbersWithUniqueDigits.java
@@ -0,0 +1,26 @@
+package work.huangdu.exploration.start_from_scratch.binary.number_bit_operation;
+
+/**
+ * 357. 计算各个位数不同的数字个数
+ * 给定一个非负整数 n，计算各位数字都不同的数字 x 的个数，其中 0 ≤ x < 10^n 。
+ * 示例:
+ * 输入: 2
+ * 输出: 91
+ * 解释: 答案应为除去 11,22,33,44,55,66,77,88,99 外，在 [0,100) 区间内的所有数字。
+ *
+ * @author duhuang@iflytek.com
+ * @version 2020/10/19 15:34
+ */
+public class CountNumbersWithUniqueDigits {
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) return 1;
+        if (n == 1) return 10;
+        if (n > 10) return 0;
+        int res = 9, i = 9, count = 1;
+        while (count < n) {
+            res *= i--;
+            count++;
+        }
+        return res + countNumbersWithUniqueDigits(n - 1);
+    }
+}
