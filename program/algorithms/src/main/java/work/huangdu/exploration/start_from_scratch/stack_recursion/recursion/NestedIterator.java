1b89d8052c759bd772351688f4448371dcbe6f08
huangdu94 <tiancaihuangdu@gmail.com>
2020-10-26 14:08:22 +0800

    341. 扁平化嵌套列表迭代器

---
+++ program/algorithms/src/main/java/work/huangdu/exploration/start_from_scratch/stack_recursion/recursion/NestedIterator.java
@@ -0,0 +1,9 @@
+package work.huangdu.exploration.start_from_scratch.stack_recursion.recursion;
+
+/**
+ * @author duhuang@iflytek.com
+ * @version 2020/10/26 14:07
+ * @see work.huangdu.exploration.advanced_algorithms.design.NestedIterator
+ */
+public class NestedIterator {
+}
