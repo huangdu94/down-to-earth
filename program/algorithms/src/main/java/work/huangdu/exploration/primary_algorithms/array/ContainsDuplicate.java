96404ebd22f4d33da2a9525238819c12c61521bc
huangdu94 <tiancaihuangdu@gmail.com>
2020-11-03 20:58:34 +0800

    每日一题。

--- program/algorithms/src/main/java/work/huangdu/exploration/primary_algorithms/array/ContainsDuplicate.java
+++ program/algorithms/src/main/java/work/huangdu/exploration/primary_algorithms/array/ContainsDuplicate.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * 存在重复元素
+ * 217. 存在重复元素
  * 给定一个整数数组，判断是否存在重复元素。
  * 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
  * 示例 1:
---
+++ program/algorithms/src/main/java/work/huangdu/question_bank/easy/ValidMountainArray.java
@@ -0,0 +1,39 @@
+package work.huangdu.question_bank.easy;
+
+/**
+ * 941. 有效的山脉数组
+ * 给定一个整数数组 A，如果它是有效的山脉数组就返回 true，否则返回 false。
+ * 让我们回顾一下，如果 A 满足下述条件，那么它是一个山脉数组：
+ * A.length >= 3
+ * 在 0 < i < A.length - 1 条件下，存在 i 使得：
+ * A[0] < A[1] < ... A[i-1] < A[i]
+ * A[i] > A[i+1] > ... > A[A.length - 1]
+ * 示例 1：
+ * 输入：[2,1]
+ * 输出：false
+ * 示例 2：
+ * 输入：[3,5,5]
+ * 输出：false
+ * 示例 3：
+ * 输入：[0,3,2,1]
+ * 输出：true
+ * 提示：
+ * 0 <= A.length <= 10000
+ * 0 <= A[i] <= 10000
+ *
+ * @author duhuang@iflytek.com
+ * @version 2020/11/3 16:32
+ */
+public class ValidMountainArray {
+    public boolean validMountainArray(int[] A) {
+        int n = A.length, i = 0, j = n - 1;
+        while (i + 1 < n && A[i] < A[i + 1]) {
+            i++;
+        }
+        if (i == 0 || i == n - 1) return false;
+        while (j - 1 >= 0 && A[j - 1] > A[j]) {
+            j--;
+        }
+        return i == j;
+    }
+}
