55051a9a85cb87f265ba62dc73fca16f173dbb18
huangdu94 <tiancaihuangdu@gmail.com>
2020-11-14 13:08:33 +0800

    修正一处注释。

--- program/java-api-demo/src/main/java/reference/PhantomReferenceDemo.java
+++ program/java-api-demo/src/main/java/reference/PhantomReferenceDemo.java
@@ -7,7 +7,7 @@ import java.lang.ref.ReferenceQueue;
  * 五、虚引用
  * 1.虚引用是所有引用类型中最弱的一个
  * 2.随时会被GC回收
- * 3.get方法取得强引用时总是会返回null
+ * 3.get方法取得虚引用时总是会返回null
  * 4.和引用队列一起使用，作用在于跟踪垃圾回收过程
  *
  * @author duhuang@iflytek
