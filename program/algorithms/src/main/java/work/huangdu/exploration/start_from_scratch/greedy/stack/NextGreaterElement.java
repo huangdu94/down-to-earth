0261abff988d467169640b11625b43e639e6b749
huangdu94 <tiancaihuangdu@gmail.com>
2020-11-20 13:43:38 +0800

    每日一题 贪心 一题。

---
+++ program/algorithms/src/main/java/work/huangdu/exploration/start_from_scratch/greedy/stack/NextGreaterElement.java
@@ -0,0 +1,73 @@
+package work.huangdu.exploration.start_from_scratch.greedy.stack;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 496. 下一个更大元素 I
+ * 给定两个 没有重复元素 的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。找到 nums1 中每个元素在 nums2 中的下一个比其大的值。
+ * nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出 -1 。
+ * 示例 1:
+ * 输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+ * 输出: [-1,3,-1]
+ * 解释:
+ * 对于num1中的数字4，你无法在第二个数组中找到下一个更大的数字，因此输出 -1。
+ * 对于num1中的数字1，第二个数组中数字1右边的下一个较大数字是 3。
+ * 对于num1中的数字2，第二个数组中没有下一个更大的数字，因此输出 -1。
+ * 示例 2:
+ * 输入: nums1 = [2,4], nums2 = [1,2,3,4].
+ * 输出: [3,-1]
+ * 解释:
+ * 对于 num1 中的数字 2 ，第二个数组中的下一个较大数字是 3 。
+ * 对于 num1 中的数字 4 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+ * 提示：
+ * nums1和nums2中所有元素是唯一的。
+ * nums1和nums2 的数组大小都不超过1000。
+ *
+ * @author duhuang@iflytek.com
+ * @version 2020/11/20 13:23
+ */
+public class NextGreaterElement {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        // key value: nums2中key右边第一个比key大的数
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[nums1.length];
+        int n = nums2.length, index = 0;
+        for (int i = 0; i < n; i++) {
+            int num = nums2[i];
+            map.put(num, -1);
+            for (int j = i + 1; j < n; j++) {
+                if (nums2[j] > num) {
+                    map.put(num, nums2[j]);
+                    break;
+                }
+            }
+        }
+        for (int num : nums1) {
+            res[index++] = map.get(num);
+        }
+        return res;
+    }
+
+    // 单调栈
+    public int[] nextGreaterElement2(int[] nums1, int[] nums2) {
+        if (nums1.length == 0) return new int[0];
+        // key value: nums2中key右边第一个比key大的数
+        int n = nums1.length, index = 0, top = 0;
+        Map<Integer, Integer> map = new HashMap<>(n);
+        int[] stack = new int[nums2.length], res = new int[n];
+        for (int num : nums2) {
+            while (top != 0 && num > stack[top - 1]) {
+                map.put(stack[--top], num);
+            }
+            stack[top++] = num;
+        }
+        while (top != 0) {
+            map.put(stack[--top], -1);
+        }
+        for (int num : nums1) {
+            res[index++] = map.get(num);
+        }
+        return res;
+    }
+}
---
+++ program/algorithms/src/main/java/work/huangdu/question_bank/medium/InsertionSortList.java
@@ -0,0 +1,99 @@
+package work.huangdu.question_bank.medium;
+
+import work.huangdu.data_structure.ListNode;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 147. 对链表进行插入排序
+ * 对链表进行插入排序。
+ * 插入排序的动画演示如上。从第一个元素开始，该链表可以被认为已经部分排序（用黑色表示）。
+ * 每次迭代时，从输入数据中移除一个元素（用红色表示），并原地将其插入到已排好序的链表中。
+ * 插入排序算法：
+ * 插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+ * 每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+ * 重复直到所有输入数据插入完为止。
+ * 示例 1：
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ * 示例 2：
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ *
+ * @author duhuang@iflytek.com
+ * @version 2020/11/20 12:45
+ */
+public class InsertionSortList {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        // 排序完链表的头，从大到小，排序完后需要翻转链表
+        ListNode sortedHead = head;
+        head = head.next;
+        sortedHead.next = null;
+        while (head != null) {
+            ListNode next = head.next, insert = sortedHead;
+            if (head.val >= insert.val) {
+                sortedHead = head;
+                head.next = insert;
+            } else {
+                while (insert.next != null && head.val < insert.next.val) {
+                    insert = insert.next;
+                }
+                head.next = insert.next;
+                insert.next = head;
+            }
+            head = next;
+        }
+        // 翻转sortedHead
+        ListNode pre = null, cur = sortedHead;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+    public ListNode insertionSortList2(ListNode head) {
+        if (head == null || head.next == null) return head;
+        // 排序完链表的头，从小到大
+        ListNode sortedHead = head;
+        head = head.next;
+        sortedHead.next = null;
+        while (head != null) {
+            ListNode next = head.next, insert = sortedHead;
+            if (head.val <= insert.val) {
+                sortedHead = head;
+                head.next = insert;
+            } else {
+                while (insert.next != null && head.val > insert.next.val) {
+                    insert = insert.next;
+                }
+                head.next = insert.next;
+                insert.next = head;
+            }
+            head = next;
+        }
+        return sortedHead;
+    }
+
+    public ListNode insertionSortList3(ListNode head) {
+        if (head == null || head.next == null) return head;
+        List<ListNode> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head);
+            head = head.next;
+        }
+        list.sort(Comparator.comparingInt(o -> o.val));
+        ListNode dummy = new ListNode(-1), cur = dummy;
+        for (ListNode node : list) {
+            cur.next = node;
+            cur = cur.next;
+        }
+        cur.next = null;
+        return dummy.next;
+    }
+}
