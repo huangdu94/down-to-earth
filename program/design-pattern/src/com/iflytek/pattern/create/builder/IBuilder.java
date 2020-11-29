bfb664dfa6eebdd70e59db971430271ce94bd1ef
huangdu94 <tiancaihuangdu@gmail.com>
2020-11-29 14:38:56 +0800

    建造者模式。

--- program/design-pattern/src/com/iflytek/pattern/create/builder/IBuilder.java
+++ program/design-pattern/src/com/iflytek/pattern/create/builder/IBuilder.java
@@ -4,14 +4,19 @@ package com.iflytek.pattern.create.builder;
  * 建造类接口
  * 理论上建造者类的方法需要先抽象为接口
  * 不过在此例子中没有体现接口的优势
+ * 可以参照https://www.jianshu.com/p/3d1c9ffb0a28中建造自行车的例子
  *
  * @author duhuang@iflytek
  * @date 2019/10/31 15:40
  */
 public interface IBuilder {
     IBuilder orderChickenBurger();
+
     IBuilder orderVegBurger();
+
     IBuilder orderPepsi();
+
     IBuilder orderCoke();
+
     Order toOrder();
 }
