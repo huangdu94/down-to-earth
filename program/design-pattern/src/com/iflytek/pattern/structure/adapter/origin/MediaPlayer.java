060aaf541d8c2de889cfd87c7bf9f30a6ee0e99d
huangdu94 <tiancaihuangdu@gmail.com>
2020-11-29 15:54:26 +0800

    适配器模式。

---
+++ program/design-pattern/src/com/iflytek/pattern/structure/adapter/origin/MediaPlayer.java
@@ -0,0 +1,11 @@
+package com.iflytek.pattern.structure.adapter.origin;
+
+/**
+ * 媒体播放器
+ *
+ * @author duhuang@iflytek.com
+ * @version 2020/11/29 15:26
+ */
+public interface MediaPlayer {
+    void play(String audioType, String fileName);
+}
