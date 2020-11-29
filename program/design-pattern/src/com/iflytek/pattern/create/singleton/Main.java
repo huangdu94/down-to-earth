7f09bdcb4251e630bbea2fb74f9c165965033267
huangdu94 <tiancaihuangdu@gmail.com>
2020-11-29 13:40:31 +0800

    三种破坏单例模式的方法。

--- program/design-pattern/src/com/iflytek/pattern/create/singleton/Main.java
+++ program/design-pattern/src/com/iflytek/pattern/create/singleton/Main.java
@@ -2,6 +2,10 @@ package com.iflytek.pattern.create.singleton;
 
 /**
  * 单例模式
+ * 破坏单例模式的三种方式。
+ * 1. 克隆 -> 只要不实现Cloneable
+ * 2. 序列化反序列化 —> 实现readResolve()方法
+ * 3. 反射 -> 加一个计数器，在构造方法里判断，如果计数器已经大于1了，构造方法再调用抛出异常
  *
  * @author DuHuang 2019/10/31 14:19
  */
