a20c2dfe365d2ac90e8b9aafb66584f0e8f84b7a
huangdu94 <tiancaihuangdu@gmail.com>
2020-12-01 13:20:47 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置

--- program/algorithms/src/main/java/work/huangdu/exploration/intermediate_algorithms/sort_search/SearchRange.java
+++ program/algorithms/src/main/java/work/huangdu/exploration/intermediate_algorithms/sort_search/SearchRange.java
@@ -3,7 +3,7 @@ package work.huangdu.exploration.intermediate_algorithms.sort_search;
 import java.util.Arrays;
 
 /**
- * 在排序数组中查找元素的第一个和最后一个位置
+ * 34. 在排序数组中查找元素的第一个和最后一个位置
  * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
  * 你的算法时间复杂度必须是 O(log n) 级别。
  * 如果数组中不存在目标值，返回 [-1, -1]。
@@ -18,7 +18,7 @@ import java.util.Arrays;
  * @version 2020/7/12 15:49
  */
 public class SearchRange {
-    public int[] searchRange(int[] nums, int target) {
+    public int[] searchRange3(int[] nums, int target) {
         if (nums == null || nums.length < 1)
             return new int[]{-1, -1};
         int l = search(nums, 0, nums.length, target, true);
@@ -91,5 +91,33 @@ public class SearchRange {
         int target = 11;
         int[] result = new SearchRange().searchRange(nums, target);
         System.out.println(Arrays.toString(result));
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        if (nums.length == 0) return new int[]{-1, -1};
+        int left = binarySearch(nums, target, true);
+        if (nums[left] != target) return new int[]{-1, -1};
+        int right = binarySearch(nums, target, false);
+        return new int[]{left, nums[right] == target ? right : right - 1};
+    }
+
+    /**
+     * 二分查找法，找出target在nums中的位置，根据flag情况来区分查找的位置时开始还是结束位置
+     *
+     * @param nums   目标数组
+     * @param target 目标值
+     * @param flag   标识用于区分处理mid等于flag的行为
+     */
+    private int binarySearch(int[] nums, int target, boolean flag) {
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] > target || flag && nums[mid] == target) {
+                j = mid;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return i;
     }
 }
