7dc0b150d023d51716e792c399e321d0c0c8a587
huangdu94 <tiancaihuangdu@gmail.com>
2020-12-05 11:38:33 +0800

    85. 最大矩形

--- program/algorithms/src/main/java/work/huangdu/exploration/start_from_scratch/greedy/stack/MaximalRectangle.java
+++ program/algorithms/src/main/java/work/huangdu/exploration/start_from_scratch/greedy/stack/MaximalRectangle.java
@@ -27,6 +27,7 @@ package work.huangdu.exploration.start_from_scratch.greedy.stack;
  *
  * @author duhuang@iflytek.com
  * @version 2020/12/4 13:31
+ * @see work.huangdu.exploration.advanced_algorithms.other.LargestRectangleArea
  */
 public class MaximalRectangle {
     public int maximalRectangle(char[][] matrix) {
