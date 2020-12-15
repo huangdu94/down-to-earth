e544b9af6de93e27e502d8027256bba69b72948f
huangdu94 <tiancaihuangdu@gmail.com>
2020-12-15 17:30:46 +0800

    双指针若干题。

---
+++ program/algorithms/src/main/java/work/huangdu/exploration/start_from_scratch/double_pointer/homonymous_sliding_window/FindMaxAverage.java
@@ -0,0 +1,36 @@
+package work.huangdu.exploration.start_from_scratch.double_pointer.homonymous_sliding_window;
+
+/**
+ * 643. 子数组最大平均数 I
+ * 给定 n 个整数，找出平均数最大且长度为 k 的连续子数组，并输出该最大平均数。
+ * 示例 1:
+ * 输入: [1,12,-5,-6,50,3], k = 4
+ * 输出: 12.75
+ * 解释: 最大平均数 (12-5-6+50)/4 = 51/4 = 12.75
+ * 注意:
+ * 1 <= k <= n <= 30,000。
+ * 所给数据范围 [-10,000，10,000]。
+ *
+ * @author duhuang@iflytek.com
+ * @version 2020/12/15 12:30
+ */
+public class FindMaxAverage {
+    public double findMaxAverage(int[] nums, int k) {
+        int n = nums.length, max = 0, sum = 0, i = 0;
+        for (; i < k; i++) {
+            sum += nums[i];
+        }
+        max = sum;
+        for (; i < n; i++) {
+            sum = sum - nums[i - k] + nums[i];
+            if (max < sum) {
+                max = sum;
+            }
+        }
+        return 1.0 * max / k;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FindMaxAverage().findMaxAverage(new int[]{1, 12, -5, -6, 50, 3}, 4));
+    }
+}
---
+++ program/algorithms/src/main/java/work/huangdu/exploration/start_from_scratch/double_pointer/homonymous_sliding_window/MinSubArrayLen.java
@@ -0,0 +1,65 @@
+package work.huangdu.exploration.start_from_scratch.double_pointer.homonymous_sliding_window;
+
+import java.util.Arrays;
+
+/**
+ * 209. 长度最小的子数组
+ * 给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的 连续 子数组，并返回其长度。如果不存在符合条件的子数组，返回 0。
+ * 示例：
+ * 输入：s = 7, nums = [2,3,1,2,4,3]
+ * 输出：2
+ * 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+ * 进阶：
+ * 如果你已经完成了 O(n) 时间复杂度的解法, 请尝试 O(n log n) 时间复杂度的解法。
+ *
+ * @author duhuang@iflytek.com
+ * @version 2020/12/15 14:40
+ */
+public class MinSubArrayLen {
+    public int minSubArrayLen(int s, int[] nums) {
+        int n = nums.length, min = n + 1, i = 0, j = 0, sum = 0;
+        while (j < n || sum >= s) {
+            if (sum < s) {
+                sum += nums[j++];
+            }
+            if (sum >= s) {
+                min = Math.min(min, j - i);
+                sum -= nums[i++];
+            }
+        }
+        return min == n + 1 ? 0 : min;
+    }
+
+    public static void main(String[] args) {
+        MinSubArrayLen msal = new MinSubArrayLen();
+        System.out.println(msal.minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3}));
+        System.out.println(msal.minSubArrayLen2(1000, new int[]{2, 3, 1, 2, 4, 3}));
+    }
+
+    public int minSubArrayLen2(int s, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        int[] sums = new int[n + 1];
+        // 为了方便计算，令 size = n + 1
+        // sums[0] = 0 意味着前 0 个元素的前缀和为 0
+        // sums[1] = A[0] 前 1 个元素的前缀和为 A[0]
+        // 以此类推
+        for (int i = 1; i <= n; i++) {
+            sums[i] = sums[i - 1] + nums[i - 1];
+        }
+        for (int i = 1; i <= n; i++) {
+            int target = s + sums[i - 1];
+            int bound = Arrays.binarySearch(sums, target);
+            if (bound < 0) {
+                bound = -bound - 1;
+            }
+            if (bound <= n) {
+                ans = Math.min(ans, bound - (i - 1));
+            }
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+}
