e0b7e9d904cf13e32868d699904a61cdcf764ead
huangdu <huangdu.hd@alibaba-inc.com>
2021-02-03 15:07:10 +0800

    每日一题。 滑动窗口。

--- program/algorithms/src/main/java/work/huangdu/question_bank/difficult/MedianSlidingWindow.java
+++ program/algorithms/src/main/java/work/huangdu/question_bank/difficult/MedianSlidingWindow.java
@@ -1,8 +1,6 @@
 package work.huangdu.question_bank.difficult;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * 480. 滑动窗口中位数
@@ -84,8 +82,112 @@ public class MedianSlidingWindow {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 2, 3, 1, 4, 2};
         int k = 3;
+//        int[] nums = {-2147483648, -2147483648, 2147483647, -2147483648, -2147483648, -2147483648, 2147483647, 2147483647, 2147483647, 2147483647, -2147483648, 2147483647, -2147483648};
+//        int k = 3;
         MedianSlidingWindow msw = new MedianSlidingWindow();
-        double[] result = msw.medianSlidingWindow2(nums, k);
+        double[] result = msw.medianSlidingWindow(nums, k);
         System.out.println(Arrays.toString(result));
+    }
+
+    public double[] medianSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        DualHeap dh = new DualHeap(k);
+        for (int i = 0; i < k; ++i) {
+            dh.insert(nums[i]);
+        }
+        double[] result = new double[n - k + 1];
+        result[0] = dh.getMedian();
+        for (int i = k; i < n; ++i) {
+            dh.insert(nums[i]);
+            dh.erase(nums[i - k]);
+            result[i - k + 1] = dh.getMedian();
+        }
+        return result;
+    }
+
+    static class DualHeap {
+        // 大根堆，维护较小的一半元素
+        private final PriorityQueue<Integer> small;
+        // 小根堆，维护较大的一半元素
+        private final PriorityQueue<Integer> large;
+        // 哈希表，记录「延迟删除」的元素，key 为元素，value 为需要删除的次数
+        private final Map<Integer, Integer> delayed;
+        private final int k;
+        // small 和 large 当前包含的元素个数，需要扣除被「延迟删除」的元素
+        private int smallSize, largeSize;
+
+        public DualHeap(int k) {
+            this.small = new PriorityQueue<>(Comparator.reverseOrder());
+            this.large = new PriorityQueue<>();
+            this.delayed = new HashMap<>();
+            this.k = k;
+            this.smallSize = 0;
+            this.largeSize = 0;
+        }
+
+        public double getMedian() {
+            return (k & 1) == 1 ? small.peek() : ((double) small.peek() + large.peek()) / 2;
+        }
+
+        public void insert(int num) {
+            if (small.isEmpty() || num <= small.peek()) {
+                small.offer(num);
+                ++smallSize;
+            } else {
+                large.offer(num);
+                ++largeSize;
+            }
+            makeBalance();
+        }
+
+        public void erase(int num) {
+            delayed.merge(num, 1, Integer::sum);
+            if (num <= small.peek()) {
+                --smallSize;
+                if (num == small.peek()) {
+                    prune(small);
+                }
+            } else {
+                --largeSize;
+                if (num == large.peek()) {
+                    prune(large);
+                }
+            }
+            makeBalance();
+        }
+
+        // 不断地弹出 heap 的堆顶元素，并且更新哈希表
+        private void prune(PriorityQueue<Integer> heap) {
+            while (!heap.isEmpty()) {
+                int num = heap.peek();
+                if (delayed.containsKey(num)) {
+                    if (delayed.merge(num, -1, Integer::sum) == 0) {
+                        delayed.remove(num);
+                    }
+                    heap.poll();
+                } else {
+                    break;
+                }
+            }
+        }
+
+        // 调整 small 和 large 中的元素个数，使得二者的元素个数满足要求
+        private void makeBalance() {
+            if (smallSize > largeSize + 1) {
+                // small 比 large 元素多 2 个
+                large.offer(small.poll());
+                --smallSize;
+                ++largeSize;
+                // small 堆顶元素被移除，需要进行 prune
+                prune(small);
+            } else if (smallSize < largeSize) {
+                // large 比 small 元素多 1 个
+                small.offer(large.poll());
+                ++smallSize;
+                --largeSize;
+                // large 堆顶元素被移除，需要进行 prune
+                prune(large);
+            }
+        }
     }
 }
