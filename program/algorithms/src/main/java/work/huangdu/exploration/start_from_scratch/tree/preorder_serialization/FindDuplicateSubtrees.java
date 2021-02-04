614cf8e879876497b12ed99e5a5cf0ea2b74f79b
huangdu <huangdu.hd@alibaba-inc.com>
2021-02-04 21:34:05 +0800

    树的前序序列化

--- program/algorithms/src/main/java/work/huangdu/exploration/start_from_scratch/tree/preorder_serialization/FindDuplicateSubtrees.java
+++ program/algorithms/src/main/java/work/huangdu/exploration/start_from_scratch/tree/preorder_serialization/FindDuplicateSubtrees.java
@@ -3,7 +3,9 @@ package work.huangdu.exploration.start_from_scratch.tree.preorder_serialization;
 import work.huangdu.data_structure.TreeNode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 652. 寻找重复的子树
@@ -36,9 +38,8 @@ import java.util.List;
  * @date 2021/2/4
  */
 public class FindDuplicateSubtrees {
-    // TODO
     // 暴力法(超出容量未通过)
-    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+/*    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
         List<TreeNode> visited = new ArrayList<>(), result = new ArrayList<>();
         preorder(root, visited, result);
         return result;
@@ -70,5 +71,48 @@ public class FindDuplicateSubtrees {
         return root1 == null && root2 == null ||
                 root1 != null && root2 != null
                         && root1.val == root2.val && isSame(root1.left, root2.left) && isSame(root1.right, root2.right);
+    }*/
+
+    public List<TreeNode> findDuplicateSubtrees2(TreeNode root) {
+        Map<String, Integer> counts = new HashMap<>();
+        List<TreeNode> result = new ArrayList<>();
+        preorder(root, counts, result);
+        return result;
+    }
+
+    public void preorder(TreeNode root, Map<String, Integer> counts, List<TreeNode> result) {
+        if (root == null) return;
+        String serialization = serialize(root);
+        if (counts.merge(serialization, 1, Integer::sum) == 2) {
+            result.add(root);
+        }
+        preorder(root.left, counts, result);
+        preorder(root.right, counts, result);
+    }
+
+    private String serialize(TreeNode node) {
+        if (node == null) return "#";
+        return Integer.toString(node.val).concat(",").concat(serialize(node.left)).concat(",").concat(serialize(node.right));
+    }
+
+    private int id = 1;
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        Map<String, Integer> ids = new HashMap<>();
+        Map<Integer, Integer> counts = new HashMap<>();
+        List<TreeNode> result = new ArrayList<>();
+        preorder(root, ids, counts, result);
+        return result;
+    }
+
+    public int preorder(TreeNode root, Map<String, Integer> ids, Map<Integer, Integer> counts, List<TreeNode> result) {
+        if (root == null) return 0;
+        String serialization = Integer.toString(root.val).concat(",").concat(Integer.toString(preorder(root.left, ids, counts, result)))
+                .concat(",").concat(Integer.toString(preorder(root.right, ids, counts, result)));
+        int id = ids.computeIfAbsent(serialization, x -> this.id++);
+        if (counts.merge(id, 1, Integer::sum) == 2) {
+            result.add(root);
+        }
+        return id;
     }
 }
