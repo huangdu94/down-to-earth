6039dbb8c6a6bbde08afb5db96fbce546ece8aae
huangdu94 <tiancaihuangdu@gmail.com>
2021-02-06 15:38:12 +0800

    297

--- program/algorithms/src/main/java/work/huangdu/exploration/intermediate_algorithms/design/Codec.java
+++ program/algorithms/src/main/java/work/huangdu/exploration/intermediate_algorithms/design/Codec.java
@@ -5,7 +5,7 @@ import work.huangdu.data_structure.TreeNode;
 import java.util.*;
 
 /**
- * 二叉树的序列化与反序列化
+ * 297. 二叉树的序列化与反序列化
  * 序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
  * 请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
  * 示例:
@@ -168,6 +168,55 @@ class Codec2 {
                     }
                 }
             }
+        }
+        return root;
+    }
+}
+
+class Codec3 {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) return "";
+        StringBuilder sb = new StringBuilder();
+        preorder(root, sb);
+        return sb.substring(1);
+    }
+
+    private void preorder(TreeNode root, StringBuilder sb) {
+        sb.append(",");
+        if (root == null) {
+            sb.append("#");
+            return;
+        }
+        sb.append(root.val);
+        preorder(root.left, sb);
+        preorder(root.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data.isEmpty()) return null;
+        String[] arr = data.split(",");
+        Queue<Integer> preorder = new LinkedList<>();
+        for (String s : arr) {
+            if ("#".equals(s)) {
+                preorder.offer(null);
+            } else {
+                preorder.offer(Integer.parseInt(s));
+            }
+        }
+        return preorder(new TreeNode(preorder.poll()), preorder);
+    }
+
+    private TreeNode preorder(TreeNode root, Queue<Integer> preorder) {
+        Integer next = preorder.poll();
+        if (next != null) {
+            root.left = preorder(new TreeNode(next), preorder);
+        }
+        next = preorder.poll();
+        if (next != null) {
+            root.right = preorder(new TreeNode(next), preorder);
         }
         return root;
     }
