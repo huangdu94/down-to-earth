2a3356331eae10e11aa97bc49843f7290c17d3ca
huangdu <huangdu.hd@alibaba-inc.com>
2021-02-24 15:43:51 +0800

    更新。

--- program/design-pattern/src/com/iflytek/pattern/behavior/state/StopState.java
+++ program/design-pattern/src/com/iflytek/pattern/behavior/state/StopState.java
@@ -7,11 +7,13 @@ package com.iflytek.pattern.behavior.state;
  * @version 2020/11/29 22:51
  */
 public class StopState implements State {
+    @Override
     public void doAction(Context context) {
         System.out.println("Player is in stop state");
         context.setState(this);
     }
 
+    @Override
     public String toString() {
         return "Stop State";
     }
