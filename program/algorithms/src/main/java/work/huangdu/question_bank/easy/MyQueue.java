3ed1af1f623fc581bc34b80d541ed66f38b2d763
huangdu <huangdu.hd@alibaba-inc.com>
2021-03-05 12:52:45 +0800

    每日一题。

--- program/algorithms/src/main/java/work/huangdu/question_bank/easy/MyQueue.java
+++ program/algorithms/src/main/java/work/huangdu/question_bank/easy/MyQueue.java
@@ -79,3 +79,52 @@ public class MyQueue {
         return stack1.isEmpty();
     }
 }
+
+class MyQueue2 {
+    private final Deque<Integer> stack1;
+    private final Deque<Integer> stack2;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyQueue2() {
+        stack1 = new LinkedList<>();
+        stack2 = new LinkedList<>();
+    }
+
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+        if (stack2.isEmpty()) { reverse(); }
+        return stack2.pop();
+    }
+
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+        if (stack2.isEmpty()) { reverse(); }
+        return stack2.peek();
+    }
+
+    /**
+     * Returns whether the queue is empty.
+     */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+    private void reverse() {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+    }
+}
