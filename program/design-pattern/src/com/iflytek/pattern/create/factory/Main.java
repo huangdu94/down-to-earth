dc9e9a87911b585e6ed11f4828e875c08f093c61
依韻 <huangdu.hd@alibaba-inc.com>
2021-05-14 13:47:05 +0800

    更新简单工厂注释。

--- program/design-pattern/src/com/iflytek/pattern/create/factory/Main.java
+++ program/design-pattern/src/com/iflytek/pattern/create/factory/Main.java
@@ -4,7 +4,8 @@ import com.iflytek.pattern.create.factory.food.IFood;
 
 /**
  * 简单工厂模式
- * 此为简单工厂，实际中可以将工厂抽象出一个接口，每一个工厂实现类单独生产一种产品
+ * 此为简单工厂
+ * 另一种 工厂模式 将工厂抽象出一个接口，每一个工厂实现类单独生产一种产品
  *
  * @author DuHuang 2019/10/31 10:31
  */
