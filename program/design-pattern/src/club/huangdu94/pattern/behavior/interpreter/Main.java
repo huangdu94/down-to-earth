90c6b8380a9a1cd79762e4dfc30333ee6bef2d8b
依韻 <huangdu.hd@alibaba-inc.com>
2021-05-15 18:35:17 +0800

    解释器模式.

--- program/design-pattern/src/club/huangdu94/pattern/behavior/interpreter/Main.java
+++ program/design-pattern/src/club/huangdu94/pattern/behavior/interpreter/Main.java
@@ -3,6 +3,9 @@ package club.huangdu94.pattern.behavior.interpreter;
 /**
  * 解释器模式
  * 解释器模式（Interpreter Pattern）提供了评估语言的语法或表达式的方式，它属于行为型模式。这种模式实现了一个表达式接口，该接口解释一个特定的上下文。这种模式被用在 SQL 解析、符号处理引擎等。
+ * 典型例子：
+ * 1. 正则表达式
+ * 2. 编译原理中，一个算术表达式通过词法分析器形成词法单元，而后这些词法单元再通过语法分析器构建语法分析树，最终形成一颗抽象的语法分析树
  *
  * @author duhuang@iflytek.com
  * @version 2020/11/29 21:32
@@ -29,6 +32,6 @@ public class Main {
 
         System.out.println("John is male? " + isMale.interpret("John"));
         System.out.println("Julie is a married women? "
-                + isMarriedWoman.interpret("Married Julie"));
+            + isMarriedWoman.interpret("Married Julie"));
     }
 }
