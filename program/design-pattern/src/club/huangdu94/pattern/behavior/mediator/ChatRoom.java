fd87ae46de21de6238c2cf474dcc9e0557289bb8
依韻 <huangdu.hd@alibaba-inc.com>
2021-05-15 18:47:37 +0800

    中介者模式.

--- program/design-pattern/src/club/huangdu94/pattern/behavior/mediator/ChatRoom.java
+++ program/design-pattern/src/club/huangdu94/pattern/behavior/mediator/ChatRoom.java
@@ -5,6 +5,7 @@ import java.util.Date;
 /**
  * 中介者
  * 聊天室
+ * 如果需要修改聊天的格式，直接在中介者类统一修改即可
  *
  * @author duhuang@iflytek.com
  * @version 2020/11/29 21:55
@@ -12,6 +13,6 @@ import java.util.Date;
 public class ChatRoom {
     public static void showMessage(User user, String message) {
         System.out.println(new Date().toString()
-                + " [" + user.getName() + "] : " + message);
+            + " [" + user.getName() + "] : " + message);
     }
 }
