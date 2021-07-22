a7eedc97782f53168faba22646722a62aa522937
依韻 <huangdu.hd@alibaba-inc.com>
2021-07-22 19:01:00 +0800

    注释修改

--- program/design-pattern/src/club/huangdu94/pattern/behavior/strategy/Main.java
+++ program/design-pattern/src/club/huangdu94/pattern/behavior/strategy/Main.java
@@ -4,7 +4,7 @@ package club.huangdu94.pattern.behavior.strategy;
  * 策略模式
  * 在策略模式（Strategy Pattern）中，一个类的行为或其算法可以在运行时更改。这种类型的设计模式属于行为型模式。
  * 在策略模式中，我们创建表示各种策略的对象和一个行为随着策略对象改变而改变的 context 对象。策略对象改变 context 对象的执行算法。
- * 创建类构造方法桥接模式和这个有点像。
+ * 结构型构造方法桥接模式和这个有点像。
  *
  * @author duhuang@iflytek.com
  * @version 2020/11/29 23:09
