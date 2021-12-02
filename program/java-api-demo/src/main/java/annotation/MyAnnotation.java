43d62f430e4d78bbf337bf6132c8de2b9ea18ec0
huangdu.hd <huangdu.hd@alibaba-inc.com>
2021-12-02 17:18:21 +0800

    注解。

---
+++ program/java-api-demo/src/main/java/annotation/MyAnnotation.java
@@ -0,0 +1,13 @@
+package annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ * @date 2021/11/25
+ */
+@Target({ElementType.TYPE_USE})
+public @interface MyAnnotation {
+
+}
