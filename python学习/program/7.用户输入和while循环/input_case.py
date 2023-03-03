1c390205892fca3adec03f1f97084cd15299187a
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-03-03 16:29:16 +0800

    input 举例。

---
+++ python学习/program/7.用户输入和while循环/input_case.py
@@ -0,0 +1,7 @@
+prompt = "Please input your name.\n"
+name = input(prompt)
+print("Hello, " + name)
+
+prompt = "Please input your age.\n"
+age = int(input(prompt))
+print("After five years, your age is " + str(age + 5))
