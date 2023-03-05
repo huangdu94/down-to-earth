00a33b4364a35606d40a4dd0dcb68df22f20d1e9
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-03-05 20:48:09 +0800

    import case

--- python学习/program/8.函数/import_case.py
+++ python学习/program/8.函数/import_case.py
@@ -1,3 +1,25 @@
-import function_case
+# 导入整个模块
+import function
+# 导入指定函数
+from function import add
+# 使用as给函数指定别名
+from function import add as function_alias
+# 使用as给模块指定别名
+import function as module_alias
 
-function_case.add(1, 2)
+# 导入所有函数（最好不要使用）
+# from function import *
+
+function.add(1, 2)
+function.subtract(1, 2)
+function.multiply(1, 2)
+function.division(1, 2)
+
+add(1, 2)
+
+function_alias(1, 2)
+
+module_alias.add(1, 2)
+module_alias.subtract(1, 2)
+module_alias.multiply(1, 2)
+module_alias.division(1, 2)
