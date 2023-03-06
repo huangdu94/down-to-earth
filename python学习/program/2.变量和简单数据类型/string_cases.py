3c0d2b366f39a7fefcb622271613b154a2147cdb
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-03-06 10:59:02 +0800

    代码提交。

--- python学习/program/2.变量和简单数据类型/string_cases.py
+++ python学习/program/2.变量和简单数据类型/string_cases.py
@@ -10,3 +10,7 @@ print(word.lstrip() + '.')
 print(word.rstrip() + '.')
 print(word.strip() + '.')
 print('1\t2\t3\t4\n5\t6\t7\t8')
+
+# 字符串赋值的时候最前面加一个r，则后面需要转义的字符自动转义
+path = r'\a\b\c\d'
+print(path)
