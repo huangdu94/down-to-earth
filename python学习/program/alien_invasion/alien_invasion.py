13abb2e427cd8823ceee6d1fda9624544d7f3264
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-03-07 17:39:37 +0800

    绘制成群外星人。

--- python学习/program/alien_invasion/alien_invasion.py
+++ python学习/program/alien_invasion/alien_invasion.py
@@ -18,7 +18,7 @@ def run_game():
     bullets = Group()
     aliens = Group()
     # 创建外星人群
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
     # 开始游戏的主循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
--- python学习/program/alien_invasion/game_functions.py
+++ python学习/program/alien_invasion/game_functions.py
@@ -68,18 +68,37 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     pygame.display.flip()
 
 
-def create_fleet(ai_settings, screen, aliens):
-    """创建外星人群"""
-    # 创建一个外星人，并计算一行可容纳多少个外星人
-    # 外星人间距为外星人宽度
-    alien = Alien(ai_settings, screen)
-    alien_width = alien.rect.width
+def get_number_aliens_x(ai_settings, alien_width):
+    """计算每行可容纳多少个外星人"""
     available_space_x = ai_settings.screen_width - 2 * alien_width
     number_aliens_x = int(available_space_x / (2 * alien_width))
-    # 创建第一行外星人
-    for alien_number in range(number_aliens_x):
-        # 创建一个外星人并将其加入当前行
-        alien = Alien(ai_settings, screen)
-        alien.x = alien_width + 2 * alien_width * alien_number
-        alien.rect.x = alien.x
-        aliens.add(alien)
+    return number_aliens_x
+
+
+def get_number_rows(ai_settings, ship_height, alien_height):
+    """计算屏幕可容纳多少行外星人"""
+    available_space_y = ai_settings.screen_height - 3 * alien_height - ship_height
+    number_rows = int(available_space_y / (2 * alien_height))
+    return number_rows
+
+
+def create_alien(ai_settings, screen, aliens, alien_number, row_number):
+    """创建一个外星人并将其放在当前行"""
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+    aliens.add(alien)
+
+
+def create_fleet(ai_settings, screen, ship, aliens):
+    """创建外星人群"""
+    # 创建一个外星人，并计算每行可容纳多少个外星人
+    alien = Alien(ai_settings, screen)
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+    number_rows = get_number_rows(ai_settings, ship.rect.height, alien.rect.height)
+    # 创建外星人群
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            create_alien(ai_settings, screen, aliens, alien_number, row_number)
