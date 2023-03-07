271e26c23ead0d187668d67b698bc67fddc6d3d9
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-03-07 16:25:38 +0800

    创建一个外星人

--- python学习/program/alien_invasion/alien_invasion.py
+++ python学习/program/alien_invasion/alien_invasion.py
@@ -1,8 +1,10 @@
 import pygame
-from settings import Settings
-from ship import Ship
-import game_functions as gf
+
 from pygame.sprite import Group
+from settings import Settings
+import game_functions as gf
+from ship import Ship
+from alien import Alien
 
 
 def run_game():
@@ -13,6 +15,8 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
+    # 创建一个外星人
+    alien = Alien(ai_settings, screen)
     # 创建一个用于存储子弹的编组
     bullets = Group()
     # 开始游戏的主循环
@@ -20,7 +24,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 
 run_game()
--- python学习/program/alien_invasion/game_functions.py
+++ python学习/program/alien_invasion/game_functions.py
@@ -52,7 +52,7 @@ def update_bullets(bullets):
             bullets.remove(bullet)
 
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     """更新屏幕上的图像，并切换到新屏幕"""
     # 每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
@@ -61,5 +61,6 @@ def update_screen(ai_settings, screen, ship, bullets):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    alien.blitme()
     # 让最近绘制的屏幕可见
     pygame.display.flip()
