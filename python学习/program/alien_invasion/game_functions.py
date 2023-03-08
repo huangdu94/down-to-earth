30a5b7b761607bf1c06c208c6ab9d4c20d22714f
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-03-08 10:33:20 +0800

    射击外星人

--- python学习/program/alien_invasion/game_functions.py
+++ python学习/program/alien_invasion/game_functions.py
@@ -44,7 +44,7 @@ def check_keyup_events(event, ship):
         ship.moving_right = False
 
 
-def update_bullets(bullets):
+def update_bullets(bullets, aliens):
     """更新子弹的位置，并删除已消失的子弹"""
     # 更新子弹的位置
     bullets.update()
@@ -52,6 +52,8 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+    # 检查是否有子弹击中了外星人，如果是这样，就删除相应的子弹和外星人
+    pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 
 def update_screen(ai_settings, screen, ship, aliens, bullets):
