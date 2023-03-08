32376486617be98c0345a68e57f24bf9a45c5299
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-03-08 11:03:28 +0800

    加快子弹速度

--- python学习/program/alien_invasion/settings.py
+++ python学习/program/alien_invasion/settings.py
@@ -11,7 +11,7 @@ class Settings:
         # 飞船的设置
         self.ship_speed_factor = 1.5
         # 子弹设置
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
