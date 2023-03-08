4a4eff210e81d9d0f7520a54bc98a25be4791fd7
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-03-08 11:25:10 +0800

    飞船增加让自己居中的方法。

--- python学习/program/alien_invasion/ship.py
+++ python学习/program/alien_invasion/ship.py
@@ -29,6 +29,10 @@ class Ship:
         # 根据self.center更新rect对象
         self.rect.centerx = self.center
 
+    def center_ship(self):
+        """让飞船在屏幕上居中"""
+        self.center = self.screen_rect.centerx
+
     def blitme(self):
         """在指定位置绘制飞船"""
         self.screen.blit(self.image, self.rect)
