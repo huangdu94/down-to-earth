ce117f6bb3d3b8e2be65f356779703c116267dc2
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-03-08 11:41:23 +0800

    配置增加飞船几条命配置

--- python学习/program/alien_invasion/settings.py
+++ python学习/program/alien_invasion/settings.py
@@ -10,6 +10,7 @@ class Settings:
         self.bg_color = (230, 230, 230)
         # 飞船的设置
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
         # 子弹设置
         self.bullet_speed_factor = 3
         self.bullet_width = 3
