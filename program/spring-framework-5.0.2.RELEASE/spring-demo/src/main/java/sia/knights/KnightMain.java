c86bd49e4fff57187ae178cb069a2c6b6cce4dca
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-09-05 21:19:33 +0800

    代码调整。

---
+++ program/spring-framework-5.0.2.RELEASE/spring-demo/src/main/java/sia/knights/KnightMain.java
@@ -0,0 +1,14 @@
+package sia.knights;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class KnightMain {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/knight.xml");
+		Knight knight = context.getBean(Knight.class);
+		knight.embarkOnQuest();
+		context.close();
+	}
+
+}
