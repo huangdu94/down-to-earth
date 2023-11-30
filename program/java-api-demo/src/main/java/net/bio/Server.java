f704ed3531a52f282a42bb5c6bd218dde9a39d21
依韻 <huangdu.hd@alibaba-inc.com>
2023-11-30 13:03:45 +0800

    bio server代码优化。

--- program/java-api-demo/src/main/java/net/bio/Server.java
+++ program/java-api-demo/src/main/java/net/bio/Server.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.UUID;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 /**
  * @author yiyun (huangdu.hd@alibaba-inc.com)
@@ -25,24 +28,22 @@ public class Server {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        this.clientHandlerPool = new ThreadPoolExecutor(10, 100, 60, TimeUnit.SECONDS, new ArrayBlockingQueue<>(100)) {
+        this.clientHandlerPool = new ThreadPoolExecutor(10, 100, 60, TimeUnit.SECONDS, new ArrayBlockingQueue<>(100), new ThreadFactoryBuilder().setNameFormat("client-handler-%d").build()) {
             @Override
-            protected void beforeExecute(Thread t, Runnable r) {
-                System.out.println("服务器接收到新的链接。");
-            }
+            protected void beforeExecute(Thread t, Runnable r) {System.out.println("服务器接收到新的链接, " + ((ClientHandler)r).getName());}
 
             @Override
             protected void afterExecute(Runnable r, Throwable t) {
-                System.out.println("有连接从服务器断开了。");
+                System.out.println("有连接从服务器断开了, " + ((ClientHandler)r).getName());
             }
         };
-        Executors.newSingleThreadExecutor().submit(this::waitConnect);
+        new ThreadPoolExecutor(1, 1, 0, TimeUnit.MILLISECONDS, new SynchronousQueue<>(), new ThreadFactoryBuilder().setNameFormat("server").build()).submit(this::waitConnect);
     }
 
     public void waitConnect() {
         while (!stop) {
             try {
-                clientHandlerPool.submit(new ClientHandler(serverSocket.accept()));
+                clientHandlerPool.submit(new ClientHandler(UUID.randomUUID().toString(), serverSocket.accept()));
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -50,9 +51,11 @@ public class Server {
     }
 
     private static class ClientHandler implements Runnable {
+        private final String name;
         private final Socket socket;
 
-        public ClientHandler(Socket socket) {
+        public ClientHandler(String name, Socket socket) {
+            this.name = name;
             this.socket = socket;
         }
 
@@ -68,6 +71,10 @@ public class Server {
                     throw new RuntimeException(e);
                 }
             }
+        }
+
+        public String getName() {
+            return name;
         }
     }
 
