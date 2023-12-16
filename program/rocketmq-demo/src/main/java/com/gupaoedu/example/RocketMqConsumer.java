008e50a24b91a9ef16e99eb2ca47396ad1d0ee91
依韻 <huangdu.hd@alibaba-inc.com>
2023-12-16 14:13:07 +0800

    demo代码提交。

--- program/rocketmq-demo/src/main/java/com/gupaoedu/example/RocketMqConsumer.java
+++ program/rocketmq-demo/src/main/java/com/gupaoedu/example/RocketMqConsumer.java
@@ -1,12 +1,17 @@
 package com.gupaoedu.example;
 
+import java.util.List;
+
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener.*;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerOrderly;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.MessageExt;
-
-import java.util.List;
 
 /**
  * 腾讯课堂搜索【咕泡学院】
@@ -16,37 +21,37 @@ import java.util.List;
  */
 public class RocketMqConsumer {
     public static void main(String[] args) throws MQClientException {
-        DefaultMQPushConsumer consumer=
-                new DefaultMQPushConsumer("gp_consumer_group");
-        consumer.setNamesrvAddr("192.168.13.102:9876");
+        DefaultMQPushConsumer consumer =
+            new DefaultMQPushConsumer("gp_consumer_group");
+        consumer.setNamesrvAddr("localhost:9876");
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
-        consumer.subscribe("gp_test_topic","*");
+        consumer.subscribe("gp_test_topic", "*");
 
-        /*consumer.registerMessageListener(new MessageListenerConcurrently() {
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list,
                                                             ConsumeConcurrentlyContext consumeConcurrentlyContext) {
                 System.out.println("Receive Message: "+list);
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS; //签收
             }
-        });*/
+        });
 
-        consumer.registerMessageListener(new MessageListenerOrderly() {
+/*        consumer.registerMessageListener(new MessageListenerOrderly() {
             @Override
             public ConsumeOrderlyStatus consumeMessage(List<MessageExt> list, ConsumeOrderlyContext consumeOrderlyContext) {
 
-                MessageExt  messageExt=list.get(0);
+                MessageExt messageExt = list.get(0);
                 //TODO  --
                 // Throw Exceptio
                 // 重新发送该消息
                 // DLQ（通用设计）
-                if(messageExt.getReconsumeTimes()==3){  //消息重发了三次
+                if (messageExt.getReconsumeTimes() == 3) {  //消息重发了三次
                     //持久化 消息记录表
                     return ConsumeOrderlyStatus.SUCCESS; //签收
                 }
                 return ConsumeOrderlyStatus.SUCCESS; //签收
             }
-        });
+        });*/
 
         consumer.start();
 
