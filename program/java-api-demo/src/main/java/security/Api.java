9f1046862f9382cf70431b3bd714623125c3b8cd
依韻 <huangdu.hd@alibaba-inc.com>
2023-12-25 14:56:01 +0800

    增加安全相关。

---
+++ program/java-api-demo/src/main/java/security/Api.java
@@ -0,0 +1,23 @@
+package security;
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
+/**
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ */
+public class Api {
+    public static void main(String[] args) {
+        /*
+         * 启用SecurityManager有两种方式：
+         * 1. JVM参数 -Djava.security.manager -Djava.security.policy=my.policy
+         * 其中my.policy是指定的策略文件，相对路径/绝对路径均可
+         * 2. 代码中设置 System.setSecurityManager()
+         */
+        SecurityManager sm = System.getSecurityManager();
+        System.out.println(sm);
+        // 使用此方法的话，该方法可以被其它地方调用
+        // 如果不使用此方法的话，即使代码本身拥有权限，但是调用者没有权限，代码也会执行失败。
+        AccessController.doPrivileged((PrivilegedAction<Object>)() -> null);
+    }
+}
